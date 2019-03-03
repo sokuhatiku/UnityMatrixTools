67f58ad1378b9c284483ec58128e8c9ab49d718b
Sokuhatiku <sokuhatiku@gmail.com>
2019-03-04 00:34:15 +0900

    逆行列をコピーする機能作成

---
+++ Editor/MatrixEditorInspector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MatrixEditor))]
+public class MatrixEditorInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		var matrixProperty = serializedObject.FindProperty("matrix");
+		EditorGUI.BeginDisabledGroup(matrixProperty.hasMultipleDifferentValues);
+		if (GUILayout.Button("Copy Invert Matrix"))
+		{
+			var matrix = matrixProperty.GetMatrixValue().inverse;
+			EditorMatrixUtility.MatrixToClipboard(ref matrix);
+		}
+		EditorGUI.EndDisabledGroup();
+	}
+}
