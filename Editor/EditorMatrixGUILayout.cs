feff4055a91a255341599be725a722c59a3c49b5
Sokuhatiku <sokuhatiku@gmail.com>
2019-03-03 22:07:17 +0900

    マテリアル用の編集フィールドを追加

--- Editor/EditorMatrixGUILayout.cs
+++ Editor/EditorMatrixGUILayout.cs
@@ -34,6 +34,35 @@ public static class EditorMatrixGUILayout
 		return EditorMatrixGUI.AdvancedMatrixField(rect, label, matrix);
 	}
 
+
+	public static void VectorMatrixField(string propertyName, Material material, string namePrefix)
+	{
+		VectorMatrixField(propertyName, material,
+			namePrefix + "0", namePrefix + "1", namePrefix + "2", namePrefix + "3");
+	}
+
+	public static void VectorMatrixField(string propertyName, Material material, string name0, string name1, string name2, string name3)
+	{
+		EditorGUI.BeginChangeCheck();
+		var field0 = material.GetVector(name0);
+		var field1 = material.GetVector(name1);
+		var field2 = material.GetVector(name2);
+		var field3 = material.GetVector(name3);
+		var matrix = new Matrix4x4(field0, field1, field2, field3);
+
+		matrix = EditorMatrixGUILayout.AdvancedMatrixField(propertyName, matrix);
+
+		var changed = EditorGUI.EndChangeCheck();
+		if (changed)
+		{
+			material.SetVector(name0, matrix.GetColumn(0));
+			material.SetVector(name1, matrix.GetColumn(1));
+			material.SetVector(name2, matrix.GetColumn(2));
+			material.SetVector(name3, matrix.GetColumn(3));
+		}
+	}
+
+
 	private static Rect GetFieldRect()
 	{
 		return GUILayoutUtility.GetRect(Screen.width, EditorMatrixGUI.AdvancedMatrixFieldHeight);
